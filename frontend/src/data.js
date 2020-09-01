736a4e889a1a33f9a423475312e8df575ac7cc50
Aidan <aidfrederick11@gmail.com>
2020-09-01 17:07:56 -0400

    Product pages working

---
+++ frontend/src/data.js
@@ -0,0 +1,23 @@
+export default {
+    products:[{
+        _id:'0',
+        name: 'Slim Fit T-shirt',
+        category: 'Shirts',
+        image: '/images/d1.jpg',
+        price:60,
+        brand: 'Adidas',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        _id:'1',
+        name: 'Comfy Jogger Pants',
+        category: 'Pants',
+        image: '/images/d1.jpg',
+        price:75,
+        brand: 'Under Armor',
+        rating: 4.3,
+        numReviews: 8
+    }
+    ]
+}
